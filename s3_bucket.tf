ad8917fed494c20faa50c0cc4355a063b491aeaa
watsonjm <jwatson@resultant.com>
2022-04-26 15:38:52 -0400

    initial commit

---
+++ s3_bucket.tf
@@ -0,0 +1,71 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+  tags   = merge(var.tags, { Name = var.bucket_name })
+}
+
+resource "aws_s3_bucket_acl" "this" {
+  bucket = aws_s3_bucket.this.id
+  acl    = var.canned_acl
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = var.versioning_enabled
+  }
+}
+
+resource "aws_s3_bucket_logging" "this" {
+  count         = var.s3_logging_bucket == null ? 0 : 1
+  bucket        = aws_s3_bucket.this.id
+  target_bucket = var.s3_logging_bucket
+  target_prefix = var.s3_logging_prefix
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  count  = var.enable_encryption ? 1 : 0
+  bucket = aws_s3_bucket.this.id
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = var.kms_key_id
+      sse_algorithm     = var.kms_key_id == null ? "AES256" : var.encryption_algorithm
+    }
+    bucket_key_enabled = true
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  count  = var.create_force_ssl_policy ? 1 : 0
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this.0.json
+}
+
+data "aws_iam_policy_document" "this" {
+  count = var.create_force_ssl_policy ? 1 : 0
+  statement {
+    sid     = "AllowSSLRequestsOnly"
+    actions = ["s3:*"]
+    effect  = "Deny"
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,83 @@
+variable "bucket_name" {
+  type        = string
+  description = "Name of your S3 bucket."
+}
+variable "tags" {
+  type        = map(any)
+  default     = null
+  description = "Map of tags."
+}
+variable "canned_acl" {
+  type        = string
+  default     = "private"
+  description = "One of AWS Canned ACLs https://docs.aws.amazon.com/AmazonS3/latest/userguide/acl-overview.html#canned-acl"
+}
+variable "versioning_enabled" {
+  type        = string
+  default     = "Enabled"
+  description = "Determines whether or not bucket versioning is enabled."
+  validation {
+    condition = anytrue([
+      var.versioning_enabled == "Enabled",
+      var.versioning_enabled == "Suspended"
+    ])
+    error_message = "Variable 'versioning_enabled' must be 'Enabled' or 'Suspended'."
+  }
+}
+variable "s3_logging_bucket" {
+  type        = string
+  default     = null
+  description = "Bucket where S3 access logs should be stored"
+}
+variable "s3_logging_prefix" {
+  type        = string
+  default     = "log/"
+  description = "Prefix used for S3 access logs."
+}
+variable "block_public_acls" {
+  type        = bool
+  default     = true
+  description = "Public access block setting."
+}
+variable "block_public_policy" {
+  type        = bool
+  default     = true
+  description = "Public access block setting."
+}
+variable "ignore_public_acls" {
+  type        = bool
+  default     = true
+  description = "Public access block setting."
+}
+variable "restrict_public_buckets" {
+  type        = bool
+  default     = true
+  description = "Public access block setting."
+}
+variable "enable_encryption" {
+  type        = bool
+  default     = true
+  description = "Determines if bucket encryption is enabled."
+}
+variable "encryption_algorithm" {
+  type        = string
+  default     = "AES256"
+  description = "The server-side encryption algorithm to use. Valid values are 'AES256' and 'aws:kms'."
+  validation {
+    condition = anytrue([
+      var.encryption_algorithm == "AES256",
+      var.encryption_algorithm == "aws:kms"
+    ])
+    error_message = "Variable 'encryption_algorithm' must be 'AES256' or 'aws:kms'."
+  }
+}
+variable "kms_key_id" {
+  type        = string
+  default     = null
+  description = "KMS key ID to use for encryption. If no key is given, then it will use default AWS SSE key."
+}
+variable "create_force_ssl_policy" {
+  type        = bool
+  default     = false
+  description = "This will create a bucket policy forcing SSL. Set to false if you need to use any other policy, including one that includes it's own SSL forcing."
+}
